fc3009cba593bf4692f06d8f5d2fe92d00dadda1
Ivan Rocha <ivan.rocha.0987@gmail.com>
2023-10-10 20:51:31 -0300

    Commit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,29 @@
 // toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
 
 // scroll sections
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () => {
+let sections = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a')
 
+onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY
+    let offset = sec.offsetTop - 100
+    let height = sec.offsetHeight
+    let id = sec.getAttribute('id')
 
-    // sticky header
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active')
+        document
+          .querySelector('header nav a[href*=' + id + ']')
+          .classList.add('active')
+      })
+    }
+  })
 
+  // sticky header
+  let header = document.querySelector('header')
 
-    // remove toggle icon and navbar when click navbar links (scroll)
-
-
-    // animation footer on scroll
-
+  header.classList.toggle('sticky', window.scrollY > 100)
 }
